8f852378d088868b7297c53a22349b258956ab39
bboooyaho <rhkahrake100@naver.com>
2024-05-15 17:53:49 +0900

    init commit

---
+++ albumshop/src/main/java/bangshop/music/run/Application.java
@@ -0,0 +1,12 @@
+package bangshop.music.run;
+
+
+import bangshop.music.view.menu.mainMenu;
+
+public class Application {
+    public static void main(String[] args) {
+
+        //메인메뉴로 이동
+        new mainMenu().mainView();
+    }
+}
---
+++ albumshop/src/main/java/bangshop/music/view/menu/mainMenu.java
@@ -0,0 +1,10 @@
+package bangshop.music.view.menu;
+
+//메인메뉴
+public class mainMenu {
+
+    public void mainView(){
+
+    }
+
+}
