bef9cdc4206ac7219a5f6f654434233a6607f82a
Jaehyeok Song <speter650@gmail.com>
2024-05-18 23:39:07 +0900

    mybatis template추가 및 파일 수정

---
+++ albumshop/src/main/java/bangshop/music/view/menu/StoreManagerView.java
@@ -0,0 +1,29 @@
+package bangshop.music.view.menu;
+
+import bangshop.music.common.utils.IOUtils;
+
+public class StoreManagerView {
+    public void storeManagerMenu() {
+        while (true) {
+            displayMenu();
+            String inputMenu = IOUtils.input("메뉴를 입력하세요: ");
+            StorageAdminMenu menu = StorageAdminMenu.from(inputMenu);
+
+            switch (menu) {
+//                case SEARCH_ALBUM ->//TODO: 앨범 검색 및 조회
+//                case ORDERS ->//TODO: 앨범 주문
+//                case STOCK_INFO ->//TODO: 앨범 재고 조회
+                case LOG_OUT -> {
+                    return;
+                }
+            }
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("==========점장메뉴 메뉴==========");
+        for (StoreManagerMenu menu : StoreManagerMenu.values()) {
+            System.out.println(menu.getCode() + ". " + menu.getTitle());
+        }
+    }
+}
