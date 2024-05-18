e25f756e7c5ca4f69d8e519b40690f54227d5256
Rohrok <yr951103@gmail.com>
2024-05-18 18:23:35 +0900

    #17 - 창고관리자 메뉴 구현
    - TODO 리스트 작성

--- albumshop/src/main/java/bangshop/music/view/menu/StorageAdminView.java
+++ albumshop/src/main/java/bangshop/music/view/menu/StorageAdminView.java
@@ -5,8 +5,23 @@ import bangshop.music.common.utils.IOUtils;
 public class StorageAdminView {
 
     public void storageAdminMenu() {
-        displayMenu();
-        String inputMenu = IOUtils.input("메뉴를 입력하세요: ");
+        while (true) {
+            displayMenu();
+            String inputMenu = IOUtils.input("메뉴를 입력하세요: ");
+            StorageAdminMenu menu = StorageAdminMenu.from(inputMenu);
+
+            switch (menu) {
+//                case STORAGE_STOCK ->//TODO: 앨범 재고 확인
+//                case ORDERS ->//TODO: 주문 내역 조회
+//                case STOCK_IN ->//TODO: 앨범 입고
+//                case STOCK_IN_LIST ->//TODO: 앨범 입고 내역 조회
+//                case STOCK_OUT -> //TODO: 앨범 출고
+//                case STOCK_OUT_LIST ->//TODO: 앨범 출고 내역 조회
+                case LOG_OUT -> {
+                    return;
+                }
+            }
+        }
     }
 
     private static void displayMenu() {
