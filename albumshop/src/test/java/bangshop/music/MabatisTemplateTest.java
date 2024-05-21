452481f5d1da196630d4fa47ebcccc0aa02f16cc
bboooyaho <rhkahrake100@naver.com>
2024-05-21 21:40:46 +0900

    #65 db-test 코드 작성

--- albumshop/src/test/java/bangshop/music/MabatisTemplateTest.java
+++ albumshop/src/test/java/bangshop/music/MabatisTemplateTest.java
@@ -1,2 +1,18 @@
-package bangshop.music;public class MabatisTemplateTest {
+package bangshop.music;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static bangshop.music.common.MyBatisTemplate.getSqlSession;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MabatisTemplateTest {
+
+    @DisplayName("SqlSesstion객체가 정상적으로 가져올 수 있다.")
+    @Test
+    void test(){
+        SqlSession sqlSession = getSqlSession();
+        assertThat(sqlSession).isNotNull();
+    }
 }
